2e92263346070c950a88ec41ba1e55b8acc63743
202601695142alunosestaciobr <202601695142@alunos.estacio.br>
2026-02-11 22:47:01 -0300

    Primeira commint: Aula de C Estacio

---
+++ OLAMUNDOS.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //Inclusão da biblioteca : Padrão de entrada e saída, função printf.
+
+int main(){ //Função principal : Começa a execução. Toda aplicação em C começa pela função main.
+
+    printf("Olá Mundo!!!\n");   //Saída de dados : printf(); imprime a mensagem. O \n adiciona uma nova linha ao final da mensagem.
+
+    return 0; //Fim do programa : return 0; indica que o programa terminou com sucesso.
+
+}
