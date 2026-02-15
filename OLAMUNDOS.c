b0ac02adea414e97df5637b3a791e84f68c3e762
202601695142alunosestaciobr <202601695142@alunos.estacio.br>
2026-02-15 16:48:12 -0300

    Terceira Edição notas dos alunos com a média

--- OLAMUNDOS.c
+++ OLAMUNDOS.c
@@ -2,7 +2,8 @@
 
 int main(){ //Função principal : Começa a execução. Toda aplicação em C começa pela função main.
 
-    char nome_aluno[10] = "b", turma[1]="A";
+    char nome_aluno[20] = "Vanderson";//Para arrray utilizar "" aspas duplas.
+    char turma = 'A'; // Sempre que for um caractere no char utilizar '' aspas simples.
     int matricula = 0;
     float nota_portugues = 0, nota_matematica = 0, nota_redacao = 0, nota_ciencia = 0;
     float soma_soma_notas = 0, resultado_notas = 0;
@@ -10,11 +11,11 @@ int main(){ //Função principal : Começa a execução. Toda aplicação em C c
     printf("**CALCULADORA DE MEDIAS ENSINO MEDIO\n");   //Saída de dados : printf(); imprime a mensagem. O \n adiciona uma nova linha ao final da mensagem.
 
     printf("INFORME SEU NOME:\n");
-    scanf("%s", &nome_aluno);
+    scanf(" %s", nome_aluno); //Ao suar %s não a necessidade de colocar &
     printf ("QUAL SUA MATRICULA: \n");
     scanf("%i", &matricula);
     printf("QUAL SUA TURMA: \n");
-    scanf("%s", &turma);
+    scanf(" %c", &turma); // Sempre lembrar que ao atribuir char um caractere deve usar & e tambem espçao entre antes %c
     printf("\n *****   *****\n");
     printf("DIGITE SUA NOTA PORTUGUÊS: \n");
     scanf("%f", &nota_portugues);
@@ -28,9 +29,9 @@ int main(){ //Função principal : Começa a execução. Toda aplicação em C c
     soma_soma_notas = nota_portugues + nota_matematica + nota_redacao + nota_ciencia;
     resultado_notas = soma_soma_notas/4;
 
-    printf("\n *****   *****\n");
-    printf("NOME: %s \n MATRICULA: %d \n TURMA: %s \n ", nome_aluno, matricula, turma);
-    printf("SUMA MÉDIA É: %.2f \n", resultado_notas);
+    printf("*****   *****");
+    printf("\n NOME: %s \n MATRICULA: %d \n TURMA: %c \n ", nome_aluno, matricula, turma);
+    printf("SUMA MÉDIA: %.2f \n", resultado_notas);
 
     return 0; //Fim do programa : return 0; indica que o programa terminou com sucesso..
 
